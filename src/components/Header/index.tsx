6566aac30bee4b9d715ec6354df33a3814acf392
maswessam <maswessam@gmail.com>
2017-11-21 14:56:54 +0200

    fix camera ,audio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,51 +1,58 @@
 import * as React from 'react';
 
-import {Icon, Menu} from 'semantic-ui-react';
-
+import { Icon, Menu } from 'semantic-ui-react';
+import * as sAction from '../../actions/sidebar'
 import SideBar from '../../containers/Sidebar';
 
 export interface HeaderProps {
   heigth?: number;
   title?: string,
   version?: string,
-  style?:{}
+  style?: {},
+  onSideBarToggle?:()=>{
+
+  }
 }
 export interface HeaderState {
   /* empty */
-  open : boolean
+  open: boolean
 }
-export class Header extends React.Component < HeaderProps,
-HeaderState > {
+export class Header extends React.Component<HeaderProps,
+  HeaderState> {
   constructor(props?: HeaderProps, context?: any) {
     super(props, context);
     this.state = {
       open: false
     }
-    this.handleSave = this
-      .handleSave
-      .bind(this);
+
   }
 
-  handleSave(text : string) {}
-  handleChange() {
-    this.setState({open: true})
+  handleClick() {
+    console.log("lkkk");
+    this.setState({ open: !this.state.open })
   }
+  handleToggleRequest() {
+    console.log(this.props.onSideBarToggle)
+    this.props.onSideBarToggle()
+  }
+ 
   render() {
-    return (
+    return (<div>
       <Menu inverted style={this.props.style} attached>
         <Menu.Item position="right">
           {this.props.version}
         </Menu.Item>
         <Menu.Item position="left">
-          <Icon color='blue' name="tasks"/>
+        <Icon color='blue' name="tasks" onClick={this.handleToggleRequest.bind(this)} />
         </Menu.Item>
       </Menu>
+    </div>
     );
   }
 }
 
 export default Header;
-var styles : React.CSSProperties = {
+var styles: React.CSSProperties = {
   link: {
     color: 'white',
     textDecoration: 'none',
--- src/forms/NewComplaint.tsx
+++ src/forms/NewComplaint.tsx
@@ -3,10 +3,10 @@ import * as MasForms from '@mas.eg/mas-forms/src';
 import * as React from 'react';
 import * as ReactDom from 'react-DOM';
 import * as models from '@mas.eg/mas-data-models/server'
-
+import {Container, Segment} from 'semantic-ui-react';
 import { FieldPropTypes, FieldState } from '@mas.eg/mas-forms/src';
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
-
+import {captureAudio,captureImage,captureVideo} from './camera'
 import {Form} from '@mas.eg/mas-forms-semantic-ui';
 import { FormPropTypes } from '@mas.eg/mas-forms/src';
 
@@ -18,7 +18,10 @@ export interface NewComplaintFormProps extends FormPropTypes {
 }
 
 export interface ComplaintFormState {
-    show: boolean
+    show: boolean,
+    imagePath:string|object,
+    videoPath:string|object,
+    audioPath:string|object
 }
 var  types = [
   {
@@ -41,7 +44,12 @@ var  types = [
 export default class ComplaintForm extends React.Component<any,ComplaintFormState> {
     constructor(props) {
         super(props);
-        this.state = {  show: false};
+        this.state = {  
+            show: false,
+            imagePath:{}||"",
+            videoPath:{}||"",
+            audioPath:{}||""
+        };
         this.handelClick = this.handelClick.bind(this);
     }
     handelClick() {
@@ -50,15 +58,31 @@ export default class ComplaintForm extends React.Component<any,ComplaintFormStat
             show: !this.state.show
         });
     }
+    captureImage() {
+        var data=captureImage(function  onsucess(result){
+            this.setState({imagePath : result}) 
+        })
+    }  
+    captureVideo() {
+        var data=captureVideo(function  onsucess(result){
+            this.setState({videoPath : result})
+        })
+       }
+    captureAudio() {
+        var data=captureAudio(function  onsucess(result){
+            this.setState({audioPath : result})
+        })
+     };
     render() {
         return <div>
+            <Segment textAlign="center" >
         <Form  size="small" onValid={()=>{this.setState({show:true});console.log('valid')}} onInvalid={()=>{console.log('invalid'),this.setState({show:false})}}
             onSubmit={(data) => {
             console.dir({data});
             return data;
         }}>
          <h1>Complaint</h1>
-               <Fields.Input placeholder="Id" name="id" inline width={8}/>
+               <Fields.Input placeholder="Id" name="id"  width={8}/>
               <Fields.Group widths='equal' name="group1" >
                     <Fields.Input  name="desc" placeholder="الوصف" />
                      <Fields.Input  name="adress" placeholder="العنوان" />
@@ -66,10 +90,21 @@ export default class ComplaintForm extends React.Component<any,ComplaintFormStat
                 <Fields.Group widths='equal' name="group2">
                         <Fields.Select placeholder="النوع" options={types} name="type"/>
                         <Fields.Select placeholder="الاجراء" options={types} name="action"/>
-                </Fields.Group >
+             </Fields.Group >
+                <Fields.Group name="group3">
+                <Fields.Button onClick={this.captureImage.bind(this)} primary name="captureImage" content="captureImage" />
+                <Fields.Button onClick={this.captureVideo.bind(this)} primary name="captureImage" content="captureVideo"/>
+                <Fields.Button onClick={this.captureAudio.bind(this)} primary name="captureImage" content="captureaudio"/>
+             </Fields.Group>
+
+
+                <Fields.Input style={{display:'none'}} value={this.state.imagePath} name="imagePath" />
+                <Fields.Input style={{display:'none'}} value={this.state.audioPath} name="audioPath" />
+                <Fields.Input style={{display:'none'}} value={this.state.videoPath} name="videoPath" />
                 <Fields.Button content="submit" primary name="button" type="submit" disabled={!this.state.show}/>
 
         </Form>
+        </Segment>
     </div>
     }
 }
